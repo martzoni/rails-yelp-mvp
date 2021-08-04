df9e2b490e2e88aab64163dfbd7bcb678b8156dc
Martin Masoni <marmasoni@gmail.com>
2021-08-04 11:39:53 +0200

    finished exercise

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,39 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [ :show ]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new()
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+			redirect_to restaurant_path(@restaurant.id)
+		else
+			render :new
+		end
+    
+  end
+
+  private 
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :id,
+      :name,
+      :address,
+      :phone_number,
+      :category
+    )
+end
+end
